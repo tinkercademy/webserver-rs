a3e3ce980bfe562d615605d9cebba1915655e722
Robert <xiurobert@gmail.com>
2022-06-27 15:25:25 +0800

    feat: support writing http status codes
    yes its a string I know bad practice

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,13 @@ use std::net::{TcpListener, TcpStream};
 /// # Arguments
 /// * `stream` - The stream to write to.
 /// * `text` - The text to write.
+/// * `status` - The status code to write. (It's a string lol)
 /// # Returns
 /// * `Ok(usize)` - The number of bytes written.
 /// * `Err(io::Error)` - The error.
-fn write_txt(txt: &str, stream: &mut TcpStream) -> Result<usize, io::Error>{
-    let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: {}\r\nServer: demo-rust-http-server/0.1.0\r\n\r\n{}",
-                           txt.len(), txt);
+fn write_txt(txt: &str, stream: &mut TcpStream, status: &str) -> Result<usize, io::Error>{
+    let response = format!("HTTP/1.1 {}\r\nContent-Type: text/html\r\nContent-Length: {}\r\nServer: demo-rust-http-server/0.1.0\r\n\r\n{}",
+        status, txt.len(), txt);
     let written = stream.write(response.as_bytes());
     stream.flush().unwrap(); // probably should not unwrap but whatever
     written
@@ -39,21 +40,17 @@ fn handle_connection(mut stream: TcpStream) {
         Some(ref path) => {
             if path == &"/" {
                 let txt = std::fs::read_to_string("pages/index.html").unwrap();
-                let _ = write_txt(&txt, &mut stream);
+                let _ = write_txt(&txt, &mut stream, "200 OK");
             } else {
                 let txt = std::fs::read_to_string("pages/404.html").unwrap();
-                let _ = write_txt(&txt, &mut stream);
+                let _ = write_txt(&txt, &mut stream, "404 Not Found");
             }
         },
         None => {
             let txt = "No path";
-            let _ = write_txt(txt, &mut stream);
+            let _ = write_txt(txt, &mut stream, "400 Bad Request");
         }
     }
-    // println!("Req: {:?}", req);
-
-    // let contents = std::fs::read_to_string("pages/index.html").unwrap();
-    // let _ = write_txt(&contents, &mut stream);
 }
 
 fn main() -> std::io::Result<()> {
