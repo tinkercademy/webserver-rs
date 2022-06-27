5e1cbff6d9674141d979e333007e5dd1de0ca5cc
Robert <xiurobert@gmail.com>
2022-06-27 14:55:57 +0800

    feat: sort of works but not really, also I used an external parser booo

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,37 @@
-use std::io::Read;
+
+use std::io;
+use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+
+/// Write text to a stream. (HTTP)
+fn write_txt(txt: &str, mut stream: &TcpStream) -> Result<usize, io::Error>{
+    let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+                           txt.len(), txt);
+    let written = stream.write(response.as_bytes());
+    stream.flush().unwrap();
+    written
+}
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     let _ = stream.read(&mut buffer);
-    println!("Received: {}", String::from_utf8_lossy(&buffer[..]));
+    let mut headers = [httparse::EMPTY_HEADER; 16];
+    let mut req = httparse::Request::new(&mut headers);
+    let res = req.parse(&buffer).unwrap();
+    if res.is_partial() {
+        match req.path {
+            Some(ref path) => {
+                println!("Path: {}", path);
+            },
+            None => {
+                println!("No path");
+            }
+        }
+    }
+    println!("Req: {:?}", req);
+
+    let contents = "Hello from rust";
+    let _ = write_txt(contents, &stream);
 }
 
 fn main() -> std::io::Result<()> {
